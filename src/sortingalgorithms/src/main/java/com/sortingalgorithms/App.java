7ef70bdc83721208c8392a4b858c33fa5f955ba4
vinhmech <vincenguyen262@gmail.com>
2021-12-08 21:08:51 -0800

    final version

--- src/sortingalgorithms/src/main/java/com/sortingalgorithms/App.java
+++ src/sortingalgorithms/src/main/java/com/sortingalgorithms/App.java
@@ -7,7 +7,7 @@ package com.sortingalgorithms;
 
 /** 
  * Rewrite Bubble Sort, Selection Sort and Insertion Sort using recursion
- * @author Group  - SJCC_CIS055
+ * @author Group Vinh Nguyen, Trang Lam, Quang Nguyen  - SJCC_CIS055
  * @version 1.0 - 11/26/2021
  */
 
@@ -54,20 +54,21 @@ public static void recursiveSelectionSort(int [] arr, int arrLength, int i){
 
 
 // Recursive Insertion Sort
-public static void recursiveInsertionSort(int [] arr, int arrLength){
-    if (arrLength <= 1){
+public static void recursiveInsertionSort(int [] arr, int i){
+    if (i == arr.length){
         return;
     }
 
-    recursiveInsertionSort(arr, arrLength - 1);
-
-    int val = arr[arrLength - 1];
-    int pos = arrLength - 2;
-    while(pos >= 0 && val > arr[pos]){
-        arr[pos + 1] = arr[pos];
-        pos = pos - 1;
+    if(i < arr.length){
+        int temp = arr[i];
+        int j = i;
+        while(j > 0 && arr[j-1] <= temp){
+            arr[j] = arr[j-1];
+            j--;
+        }
+        arr[j] = temp;
     }
-    arr[pos + 1] = val;
+    recursiveInsertionSort(arr, i + 1);
 }
 
 
@@ -76,6 +77,7 @@ public static void printArray(int [] arr){
     for (int i = 0; i < arr.length; i++){
         System.out.printf("%d ", arr[i]);
     }
+    System.out.println();
 }
 
 // main function
@@ -85,16 +87,26 @@ public static void main( String[] args )
         int testArray2[] = {3,1,5,8,2,6,4,9,7};
         int testArray3[] = {6,3,7,2,1,6,5,4,9};
 
+        System.out.println("testArray1 before recursive bubble sort: ");
+        printArray(testArray1);
         recursiveBubbleSort(testArray1, testArray1.length);
-        System.out.println("testArray1: ");
+        System.out.println("testArray1 after recursive bubble sort: ");
         printArray(testArray1);
 
+        System.out.println();
+
+        System.out.println("testArray2 before recursive selection sort: ");
+        printArray(testArray2);
         recursiveSelectionSort(testArray2, testArray2.length, 0);
-        System.out.println("\ntestArray2: ");
+        System.out.println("testArray2 after recursive selection sort: ");
         printArray(testArray2);
 
-        recursiveInsertionSort(testArray3, testArray3.length);
-        System.out.println("\ntestArray3: ");
+        System.out.println();
+
+        System.out.println("testArray3 before recursive insertion sort: ");
+        printArray(testArray3);
+        recursiveInsertionSort(testArray3, 0);
+        System.out.println("testArray3 after recursive insertion sort: ");
         printArray(testArray3);
     }
 }
