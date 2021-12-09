88f7b4c530d5ef60f3b3c5bc2ea28ac09c0b33fc
Dang Quang Nguyen <quangnguyen1705@gmail.com>
2021-12-09 12:23:35 -0800

    Update recursiveInsertionSort
    - update recursiveInsertionSort and main function

--- src/sortingalgorithms/src/main/java/com/sortingalgorithms/App.java
+++ src/sortingalgorithms/src/main/java/com/sortingalgorithms/App.java
@@ -7,7 +7,7 @@ package com.sortingalgorithms;
 
 /** 
  * Rewrite Bubble Sort, Selection Sort and Insertion Sort using recursion
- * @author Group Vinh Nguyen, Trang Lam, Quang Nguyen  - SJCC_CIS055
+ * @author Group  - SJCC_CIS055
  * @version 1.0 - 11/26/2021
  */
 
@@ -51,24 +51,29 @@ public static void recursiveSelectionSort(int [] arr, int arrLength, int i){
     recursiveSelectionSort(arr, arrLength, i + 1);
 }
 
-
-
 // Recursive Insertion Sort
-public static void recursiveInsertionSort(int [] arr, int i){
-    if (i == arr.length){
-        return;
-    }
+public static void recursiveInsertionSort(int [] arr, int arrLength){
+	// Base case
+	if (n <= 1)
+		return;
 
-    if(i < arr.length){
-        int temp = arr[i];
-        int j = i;
-        while(j > 0 && arr[j-1] <= temp){
-            arr[j] = arr[j-1];
-            j--;
-        }
-        arr[j] = temp;
-    }
-    recursiveInsertionSort(arr, i + 1);
+	//Call recursive function with n-1 element
+	insertionSortRecursive(arr, n - 1);
+
+	// Insert last element at its correct position
+	// in sorted array.
+	int last = arr[n - 1];
+	int j = n - 2;
+
+	/*
+	* Move elements of arr[0..i-1], that are greater than key, to one position
+	* ahead of their current position
+	*/
+	while (j >= 0 && arr[j] < last) {
+		arr[j + 1] = arr[j];
+		j--;
+	}
+	arr[j + 1] = last;
 }
 
 
@@ -77,37 +82,48 @@ public static void printArray(int [] arr){
     for (int i = 0; i < arr.length; i++){
         System.out.printf("%d ", arr[i]);
     }
-    System.out.println();
 }
 
 // main function
 public static void main( String[] args )
     {
+    	// nomarl interge number
         int testArray1[] = {2,4,7,5,8,5,3,9,6};
-        int testArray2[] = {3,1,5,8,2,6,4,9,7};
+        // negative number and zero number
+        int testArray2[] = {3,1,5,0,2,-6,4,9,7};
+        // dulpicate number
         int testArray3[] = {6,3,7,2,1,6,5,4,9};
 
-        System.out.println("testArray1 before recursive bubble sort: ");
-        printArray(testArray1);
         recursiveBubbleSort(testArray1, testArray1.length);
-        System.out.println("testArray1 after recursive bubble sort: ");
+        System.out.println("testArray1: ");
         printArray(testArray1);
 
-        System.out.println();
-
-        System.out.println("testArray2 before recursive selection sort: ");
-        printArray(testArray2);
         recursiveSelectionSort(testArray2, testArray2.length, 0);
-        System.out.println("testArray2 after recursive selection sort: ");
+        System.out.println("\ntestArray2: ");
         printArray(testArray2);
 
-        System.out.println();
+        System.out.println("====================TEST CASE 1=====================");
+		System.out.print("Before Insertion Sort: ");
+		printArray(testArray1);
+		recursiveInsertionSort(testArray1, testArray1.length);
+		System.out.print("After Insertion Sort: ");
+		printArray(testArray1);
 
-        System.out.println("testArray3 before recursive insertion sort: ");
-        printArray(testArray3);
-        recursiveInsertionSort(testArray3, 0);
-        System.out.println("testArray3 after recursive insertion sort: ");
-        printArray(testArray3);
+		System.out.println("====================TEST CASE 2=====================");
+		System.out.print("Before Insertion Sort: ");
+		printArray(testArray2);
+		recursiveInsertionSort(testArray2, testArray2.length);
+		System.out.print("After Insertion Sort: ");
+		printArray(testArray2);
+
+		System.out.println("====================TEST CASE 3=====================");
+		System.out.print("Before Insertion Sort: ");
+		printArray(testArray3);
+		recursiveInsertionSort(testArray3, testArray3.length);
+		System.out.print("After Insertion Sort: ");
+		printArray(testArray3);
+
+      
     }
 }
 
